e72351cb0bfc4daaafa206010342123d5447cd90
whong <1239918017@qq.com>
2017-10-24 10:59:51 +0800

    直接页面访问方法

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,5 +12,15 @@ public class MainController {
     @RequestMapping("/")
     public String homes(){
         return "front/index";
+    }
+    
+    @RequestMapping("/gotoFront/{page}")
+    public String gotoFront(@PathVariable String page){
+        return "front/"+page;
+    }
+    
+    @RequestMapping("/gotoBackstage/{page}")
+    public String gotoBackstage(@PathVariable String page){
+        return "backstage/"+page;
     }
 }
