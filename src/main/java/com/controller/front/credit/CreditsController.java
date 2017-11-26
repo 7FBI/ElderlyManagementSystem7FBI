2375b595450ce1fa49453b3748e50be9e3c4bb86
aabbccddee123 <1647976767@qq.com>
2017-11-26 09:54:02 +0800

    增加积分2

---
+++ src/main/java/com/controller/front/credit/CreditsController.java
@@ -0,0 +1,65 @@
+package com.controller.front.credit;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bean.Credit;
+import com.bean.OldUsers;
+import com.service.CreditService;
+
+@Controller
+@RequestMapping("/front/credit")
+public class CreditsController {
+	
+	@Autowired
+	@Qualifier("creditService")
+	private CreditService creditService;
+	
+	@RequestMapping("/selectCreditByUid")
+	public ModelAndView selectCreditByUid(HttpServletRequest request){
+		OldUsers oldUser = (OldUsers) request.getSession().getAttribute("oldUsers");
+		Credit credit = creditService.selectCreditByUid(oldUser.getUid());
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("credit", credit);
+		modelAndView.setViewName("front/integral");
+		modelAndView.setViewName("front/SelfCenter_CreditDetail");
+		return modelAndView;
+	}
+	
+	@RequestMapping("/updateCreditByBuy")
+	public String updateCreditByBuy(Integer credit,String uid){
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("credit", credit);
+		map.put("uid", "1827526748");
+		creditService.updateCreditByBuy(map);
+		return "redirect:/front/credit/selectCredit";
+	}
+	
+	@RequestMapping("/updateCreditByExchange")
+	public String updateCreditByExchange(Integer credit,HttpServletRequest request){
+		OldUsers oldUser = (OldUsers) request.getSession().getAttribute("oldUsers");
+		Credit credit1 = creditService.selectCreditByUid(oldUser.getUid());
+		if(credit<=credit1.getRest_Credit()){
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("credit", credit);
+			map.put("uid", oldUser.getUid());
+			creditService.updateCreditByExchange(map);
+			return "redirect:/front/credit/selectCredit";
+		}else{
+			request.setAttribute("msd", "你的积分不够");
+			return "redirect:/front/creditshop/SelectAllCreditShop";
+			
+		}	
+		
+		
+	}
+
+}
